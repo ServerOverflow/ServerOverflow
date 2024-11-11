76687e84be70159ac5303b865ccb086f45d1cd1e
TheAirBlow <theairblow@gmail.com>
2024-11-11 23:46:25 +0500

    fix obfuscated

--- ServerOverflow/wwwroot/js/site.js
+++ ServerOverflow/wwwroot/js/site.js
@@ -9,10 +9,8 @@ $(document).ready(function() {
             let len = el.text().length;
             let output = "";
             for (let i = 0; i < len; i++)
-                output += String.fromCharCode(randInt(64, 95));
-            el.find('a').each(function() {
-                $(this).text(output);
-            });
+                output += String.fromCharCode(randInt(32, 591));
+            el.text(output);
         });
     }, 50);
 
